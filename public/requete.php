f113622c660f2795c62076c695052c823b09ebc8
Baptiste PAVOT <baptistepavot@MacBook-Pro-de-Baptiste.local>
2018-10-02 17:06:50 +0200

    cc

---
+++ public/requete.php
@@ -0,0 +1,26 @@
+<?php
+require_once "../vendor/autoload.php";
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+
+$request = Request::createFromGlobals();
+$methode = $request->getMethod();
+$nom = $request->get("nom","Inconnu");
+
+$response = new Response();
+$html = <<<HTML
+<html>
+<body>
+    <h1>Bonjour {$nom}</h1>
+    La methode de la requete est <b>{$methode}</b>
+</body>
+</html>
+HTML;
+
+$response->setContent( $html );
+$response->setStatusCode(Response::HTTP_OK);
+// sets a HTTP response header
+$response->headers->set('Content-Type', 'text/html');
+// prints the HTTP headers followed by the content
+$response->send();
